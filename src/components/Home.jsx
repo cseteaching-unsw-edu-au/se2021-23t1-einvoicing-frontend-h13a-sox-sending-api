5fbed560705466c88c0efbe0faa6b93a1978cb9e
Jacqueline <126740004+jacquelinie@users.noreply.github.com>
2023-04-17 14:00:08 +1000

    Home page looks good

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,7 @@ export const Home = (props) => {
       <div className="container-center-horizontal">
         <div className="home-page screen">
           <div className="flex-col">
-            <h1 style={{position: "absolute", top: "200px", whiteSpace: "nowrap" }} className="large-text-white">Welcome! How can we help you today?
+            <h1 style={{position: "absolute", top: "100px", whiteSpace: "nowrap" }} className="large-text-white">Welcome! How can we help you today?
             </h1>
             <div className="navigation-c">
             {/* Logout Button */}
@@ -29,42 +29,40 @@ export const Home = (props) => {
             </div>
             <div>
             {/* Create Invoice Button */}
-            <home-page-button style={{position: "absolute", left: "425px", top: "400px" }} onClick={() => navigate("CreateInvoice")}>
+            <home-page-button style={{position: "absolute", left: "300px", top: "225px" }} onClick={() => navigate("CreateInvoice")}>
               <img src={create} alt="Create Invoice" />
             </home-page-button>
             </div>
             <div>
             {/* Authenticate Invoice Button */}
-            <home-page-button style={{position: "absolute", left: "725px", top: "400px" }} onClick={() => navigate("CreateInvoice")}>
+            <home-page-button style={{position: "absolute", left: "600px", top: "225px"}} onClick={() => navigate("CreateInvoice")}>
               <img src={authenticate} alt="Authenticate Invoice" />
             </home-page-button>
             </div>
             <div>
             {/* Render Invoice Button */}
-            <home-page-button style={{position: "absolute", left: "1025px", top: "400px" }} onClick={() => navigate("RenderInvoice")}>
+            <home-page-button style={{position: "absolute", left: "900px", top: "225px" }} onClick={() => navigate("RenderInvoice")}>
               <img src={render} alt="Render Invoice" />
             </home-page-button>
             </div>
             <div>
             {/* My Invoices Button */}
-            <home-page-button style={{position: "absolute", left: "425px", top: "700px" }} onClick={() => navigate("StoreInvoice")}>
+            <home-page-button style={{position: "absolute", left: "300px", top: "525px" }} onClick={() => navigate("StoreInvoice")}>
               <img src={my_invoices} alt="My Invoices" />
             </home-page-button>
             </div>
             <div>
             {/* Send Invoice Button */}
-            <home-page-button style={{position: "absolute", left: "725px", top: "700px" }} onClick={() => navigate("SendInvoice")}>
+            <home-page-button style={{position: "absolute", left: "600px", top: "525px" }} onClick={() => navigate("SendInvoice")}>
               <img src={send} alt="Send Invoice" />
             </home-page-button>
             </div>
             <div>
             {/* Inbox Button */}
-            <home-page-button style={{position: "absolute", left: "1025px", top: "700px" }} onClick={() => navigate("CreateInvoice")}>
+            <home-page-button style={{position: "absolute", left: "900px", top: "525px" }} onClick={() => navigate("CreateInvoice")}>
               <img src={inbox} alt="Inbox" />
             </home-page-button>
             </div>
-
-
           </div>
         </div>
       </div>
